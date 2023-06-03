37ebaf32b1e00cf615203f19a2e71bcf0429b837
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-06-04 02:00:22 +0300

    chore: unused functions removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
